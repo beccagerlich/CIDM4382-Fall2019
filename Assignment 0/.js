4994b4d286a10c197efee20c91b57ea249e31b4e
Becca Gerlich <becca_gerlich@Beccas-MacBook-Pro-3.local>
2019-09-02 14:10:36 -0500

    added files

---
+++ Assignment 0/.js
@@ -0,0 +1,9 @@
+function HandleSubmit(){
+    alert("DUDE");
+  }
+  
+  function HandlePress(){
+    var content = document.getElementById("Dude2").value;
+    alert(content);
+    document.getElementById("content").innerHTML = content;
+  }
